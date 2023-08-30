ee98a9678ab007e6e0d0094f70df329116162cbb
Business-Data-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2023-08-29 20:20:48 -0600

    chore: prep rust, create bin crate

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+#![allow(unused_variables, dead_code, unused_mut, unused_imports)]
+
+fn main() {
+    let mut s = String::from("hello");
+    let r1 = &mut s;
+    let r2 = &mut s;
+    // println!("{r1}");
+}
