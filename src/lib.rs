4badeea06e292a34c0c1e562f5a41466c4da9c0b
Business-Data-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2023-11-09 20:57:33 -0700

    feat: nth prime

--- src/lib.rs
+++ src/lib.rs
@@ -1,141 +1,61 @@
 #![allow(unused_variables, dead_code, unused_mut, unused_imports)]
 
-use std::collections::HashMap;
-use std::collections::HashSet;
-
-fn main() {
-    let mut s = String::from("hello");
-    let r1 = &mut s;
-    let r2 = &mut s;
-    // println!("{r1}");
-}
-
-pub fn brackets_are_balanced(string: &str) -> bool {
-    if string.is_empty() {
-        return true;
+fn is_prime(n: u32, primes: &Vec<u32>) -> bool {
+    if n <= 1 {
+        return false;
     }
-
-    let opening_brackets: HashSet<char> = HashSet::from(['{', '[', '(']);
-    let closing_brackets: HashSet<char> = HashSet::from(['}', ']', ')']);
-    let matching_bracket: HashMap<char, char> = HashMap::from([('}', '{'), (']', '['), (')', '(')]);
-
-    let mut stack: Vec<char> = Vec::new();
-
-    for character in string.chars() {
-        let is_opening_bracket: bool = opening_brackets.contains(&character);
-        let is_closing_bracket: bool = closing_brackets.contains(&character);
-
-        if !is_opening_bracket && !is_closing_bracket {
-            continue;
-        }
-
-        if is_opening_bracket {
-            stack.push(character);
-        } else if let Some(last_element) = stack.pop() {
-            if last_element != matching_bracket[&character] {
-                return false;
-            }
-        } else {
+    for p in primes {
+        if n % p == 0 {
             return false;
         }
     }
-    stack.is_empty()
+    true
 }
 
-#[test]
-fn paired_square_brackets() {
-    assert!(brackets_are_balanced("[]"));
+pub fn nth(n: u32) -> u32 {
+    let mut primes = Vec::from([2]);
+
+    let mut i = 2;
+    while primes.len() < (n + 1) as usize {
+        if is_prime(i, &primes) {
+            primes.push(i);
+        }
+        i += 1;
+    }
+    let last_prime = primes.last().expect("No primes found");
+    *last_prime
 }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn empty_string() {
-    assert!(brackets_are_balanced(""));
-}
+    #[test]
+    fn first_prime() {
+        assert_eq!(nth(0), 2);
+    }
 
-#[test]
-fn unpaired_brackets() {
-    assert!(!brackets_are_balanced("[["));
-}
+    #[test]
+    fn second_prime() {
+        assert_eq!(nth(1), 3);
+    }
 
-#[test]
-fn wrong_ordered_brackets() {
-    assert!(!brackets_are_balanced("}{"));
-}
+    #[test]
+    fn sixth_prime() {
+        assert_eq!(nth(5), 13);
+    }
 
-#[test]
-fn wrong_closing_bracket() {
-    assert!(!brackets_are_balanced("{]"));
-}
+    #[test]
+    fn big_prime() {
+        assert_eq!(nth(10_000), 104_743);
+    }
 
-#[test]
-fn paired_with_whitespace() {
-    assert!(brackets_are_balanced("{ }"));
-}
+    #[test]
+    fn is_prime_2() {
+        assert!(is_prime(2, &vec![]));
+    }
 
-#[test]
-fn partially_paired_brackets() {
-    assert!(!brackets_are_balanced("{[])"));
-}
-
-#[test]
-fn simple_nested_brackets() {
-    assert!(brackets_are_balanced("{[]}"));
-}
-
-#[test]
-fn several_paired_brackets() {
-    assert!(brackets_are_balanced("{}[]"));
-}
-
-#[test]
-fn paired_and_nested_brackets() {
-    assert!(brackets_are_balanced("([{}({}[])])"));
-}
-
-#[test]
-fn unopened_closing_brackets() {
-    assert!(!brackets_are_balanced("{[)][]}"));
-}
-
-#[test]
-fn unpaired_and_nested_brackets() {
-    assert!(!brackets_are_balanced("([{])"));
-}
-
-#[test]
-fn paired_and_wrong_nested_brackets() {
-    assert!(!brackets_are_balanced("[({]})"));
-}
-
-#[test]
-fn paired_and_incomplete_brackets() {
-    assert!(!brackets_are_balanced("{}["));
-}
-
-#[test]
-fn too_many_closing_brackets() {
-    assert!(!brackets_are_balanced("[]]"));
-}
-
-#[test]
-fn early_incomplete_brackets() {
-    assert!(!brackets_are_balanced(")()"));
-}
-
-#[test]
-fn early_mismatched_brackets() {
-    assert!(!brackets_are_balanced("{)()"));
-}
-
-#[test]
-fn math_expression() {
-    assert!(brackets_are_balanced("(((185 + 223.85) * 15) - 543)/2"));
-}
-
-#[test]
-fn complex_latex_expression() {
-    let input = "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \
-                 \\end{array}\\right)";
-
-    assert!(brackets_are_balanced(input));
+    #[test]
+    fn is_prime_3() {
+        assert!(is_prime(3, &vec![]));
+    }
 }
