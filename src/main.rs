3f9e0d514d3d2ab459be2e6f8519097544c75bd8
Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2024-12-29 10:25:07 -0700

    refactor: simplify main script

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn greet(request: HttpRequest) -> impl Responder {
-    let name = request.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check(_request: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use rust_dojo;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    rust_dojo::run().await
 }
