01e98b0ab0fc7268603179f343b49e488b285452
Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2024-12-29 18:22:33 -0700

    refactor: use HashMap to hold ratios

--- crates/exercism/src/lib.rs
+++ crates/exercism/src/lib.rs
@@ -1,6 +1,42 @@
+use std::collections::HashMap;
+
 const DAYS_PER_EARTH_YEAR: f64 = 365.25;
 const SECONDS_PER_EARTH_DAY: f64 = 24.0 * 60.0 * 60.0;
 const SECONDS_PER_EARTH_YEAR: f64 = DAYS_PER_EARTH_YEAR * SECONDS_PER_EARTH_DAY;
+
+pub struct Mercury;
+pub struct Venus;
+pub struct Earth;
+pub struct Mars;
+pub struct Jupiter;
+pub struct Saturn;
+pub struct Uranus;
+pub struct Neptune;
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+enum PlanetEnum {
+    Mercury,
+    Venus,
+    Earth,
+    Mars,
+    Jupiter,
+    Saturn,
+    Uranus,
+    Neptune,
+}
+
+fn create_planet_to_earth_orbital_period_ratio_mapping() -> HashMap<PlanetEnum, f64> {
+    let mut planet_to_earth_orbital_period_ratio_mapping = HashMap::new();
+    planet_to_earth_orbital_period_ratio_mapping.insert(PlanetEnum::Mercury, 0.2408467);
+    planet_to_earth_orbital_period_ratio_mapping.insert(PlanetEnum::Venus, 0.61519726);
+    planet_to_earth_orbital_period_ratio_mapping.insert(PlanetEnum::Earth, 1.0);
+    planet_to_earth_orbital_period_ratio_mapping.insert(PlanetEnum::Mars, 1.8808158);
+    planet_to_earth_orbital_period_ratio_mapping.insert(PlanetEnum::Jupiter, 11.862615);
+    planet_to_earth_orbital_period_ratio_mapping.insert(PlanetEnum::Saturn, 29.447498);
+    planet_to_earth_orbital_period_ratio_mapping.insert(PlanetEnum::Uranus, 84.016846);
+    planet_to_earth_orbital_period_ratio_mapping.insert(PlanetEnum::Neptune, 164.79132);
+    planet_to_earth_orbital_period_ratio_mapping
+}
 
 #[derive(Debug)]
 pub struct Duration {
@@ -19,68 +55,74 @@ pub trait Planet {
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
-
 impl Planet for Mercury {
     fn years_during(duration: &Duration) -> f64 {
         let duration_seconds = duration.time_in_seconds as f64;
+        let planet_to_earth_mapping = create_planet_to_earth_orbital_period_ratio_mapping();
+        let seconds_per_mercury_year =
+            planet_to_earth_mapping.get(&PlanetEnum::Mercury).unwrap() * SECONDS_PER_EARTH_YEAR;
 
-        duration_seconds / (0.2408467 * SECONDS_PER_EARTH_YEAR)
+        duration_seconds / seconds_per_mercury_year
     }
 }
 impl Planet for Venus {
     fn years_during(duration: &Duration) -> f64 {
         let duration_seconds = duration.time_in_seconds as f64;
-
-        duration_seconds / (0.61519726 * SECONDS_PER_EARTH_YEAR)
+        let planet_to_earth_mapping = create_planet_to_earth_orbital_period_ratio_mapping();
+        let seconds_per_venus_year =
+            planet_to_earth_mapping.get(&PlanetEnum::Venus).unwrap() * SECONDS_PER_EARTH_YEAR;
+        duration_seconds / seconds_per_venus_year
     }
 }
 impl Planet for Earth {
     fn years_during(duration: &Duration) -> f64 {
         let duration_seconds = duration.time_in_seconds as f64;
-
-        duration_seconds / SECONDS_PER_EARTH_YEAR
+        let seconds_per_earth_year = SECONDS_PER_EARTH_YEAR;
+        duration_seconds / seconds_per_earth_year
     }
 }
 impl Planet for Mars {
     fn years_during(duration: &Duration) -> f64 {
         let duration_seconds = duration.time_in_seconds as f64;
-
-        duration_seconds / (1.8808158 * SECONDS_PER_EARTH_YEAR)
+        let planet_to_earth_mapping = create_planet_to_earth_orbital_period_ratio_mapping();
+        let seconds_per_mars_year =
+            planet_to_earth_mapping.get(&PlanetEnum::Mars).unwrap() * SECONDS_PER_EARTH_YEAR;
+        duration_seconds / seconds_per_mars_year
     }
 }
 impl Planet for Jupiter {
     fn years_during(duration: &Duration) -> f64 {
         let duration_seconds = duration.time_in_seconds as f64;
-
-        duration_seconds / (11.862615 * SECONDS_PER_EARTH_YEAR)
+        let planet_to_earth_mapping = create_planet_to_earth_orbital_period_ratio_mapping();
+        let seconds_per_jupiter_year =
+            planet_to_earth_mapping.get(&PlanetEnum::Jupiter).unwrap() * SECONDS_PER_EARTH_YEAR;
+        duration_seconds / seconds_per_jupiter_year
     }
 }
 impl Planet for Saturn {
     fn years_during(duration: &Duration) -> f64 {
         let duration_seconds = duration.time_in_seconds as f64;
-
-        duration_seconds / (29.447498 * SECONDS_PER_EARTH_YEAR)
+        let planet_to_earth_mapping = create_planet_to_earth_orbital_period_ratio_mapping();
+        let seconds_per_saturn_year =
+            planet_to_earth_mapping.get(&PlanetEnum::Saturn).unwrap() * SECONDS_PER_EARTH_YEAR;
+        duration_seconds / seconds_per_saturn_year
     }
 }
 impl Planet for Uranus {
     fn years_during(duration: &Duration) -> f64 {
         let duration_seconds = duration.time_in_seconds as f64;
-
-        duration_seconds / (84.016846 * SECONDS_PER_EARTH_YEAR)
+        let planet_to_earth_mapping = create_planet_to_earth_orbital_period_ratio_mapping();
+        let seconds_per_uranus_year =
+            planet_to_earth_mapping.get(&PlanetEnum::Uranus).unwrap() * SECONDS_PER_EARTH_YEAR;
+        duration_seconds / seconds_per_uranus_year
     }
 }
 impl Planet for Neptune {
     fn years_during(duration: &Duration) -> f64 {
         let duration_seconds = duration.time_in_seconds as f64;
-
-        duration_seconds / (164.79132 * SECONDS_PER_EARTH_YEAR)
+        let planet_to_earth_mapping = create_planet_to_earth_orbital_period_ratio_mapping();
+        let seconds_per_neptune_year =
+            planet_to_earth_mapping.get(&PlanetEnum::Neptune).unwrap() * SECONDS_PER_EARTH_YEAR;
+        duration_seconds / seconds_per_neptune_year
     }
 }
