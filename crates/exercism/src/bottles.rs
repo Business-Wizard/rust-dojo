57a634043d96807ee8d92278d945c38205a3159a
Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2025-01-04 10:57:49 -0700

    refactor: handle next_num as phrase over number

--- crates/exercism/src/bottles.rs
+++ crates/exercism/src/bottles.rs
@@ -1,7 +1,7 @@
 pub fn verse(num: u32) -> String {
-    let next_num = num - 1;
     let current_container = container(num);
-    let next_container = container(next_num);
+    let next_num = next_number_word(num);
+    let next_container = container(num - 1);
     match num {
         2..=99 => format!(
             "{num} {current_container} of beer on the wall, {num} {current_container} of beer.\n\
@@ -9,7 +9,7 @@ pub fn verse(num: u32) -> String {
         ),
         1 => format!(
             "{num} {current_container} of beer on the wall, {num} {current_container} of beer.\n\
-            Take it down and pass it around, no more bottles of beer on the wall.\n"
+            Take it down and pass it around, {next_num} bottles of beer on the wall.\n"
         ),
         _ => todo!(),
     }
@@ -19,6 +19,13 @@ fn container(num: u32) -> String {
     match num {
         1 => "bottle".to_string(),
         _ => "bottles".to_string(),
+    }
+}
+
+fn next_number_word(num: u32) -> String {
+    match num {
+        1 => "no more".to_string(),
+        _ => (num - 1).to_string(),
     }
 }
 
