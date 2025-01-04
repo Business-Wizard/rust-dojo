2367d8fef822c90f2e426c649473f1f8981b5be1
Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2025-01-04 11:04:46 -0700

    refactor: extract pronoun concept into var

--- crates/exercism/src/bottles.rs
+++ crates/exercism/src/bottles.rs
@@ -1,17 +1,25 @@
 pub fn verse(num: u32) -> String {
     let current_container = container(num);
+    let pronoun = generate_pronoun(num);
     let count_phrase = count_phrase(num);
     let next_container = container(num - 1);
     match num {
         2..=99 => format!(
             "{num} {current_container} of beer on the wall, {num} {current_container} of beer.\n\
-            Take one down and pass it around, {count_phrase} {next_container} of beer on the wall.\n"
+            Take {pronoun} down and pass it around, {count_phrase} {next_container} of beer on the wall.\n"
         ),
         1 => format!(
             "{num} {current_container} of beer on the wall, {num} {current_container} of beer.\n\
-            Take it down and pass it around, {count_phrase} bottles of beer on the wall.\n"
+            Take {pronoun} down and pass it around, {count_phrase} bottles of beer on the wall.\n"
         ),
         _ => todo!(),
+    }
+}
+
+fn generate_pronoun(num: u32) -> String {
+    match num {
+        1 => "it".to_string(),
+        _ => "one".to_string(),
     }
 }
 
