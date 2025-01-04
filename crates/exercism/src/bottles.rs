42e17259ff8e33d1b4bfc6777c648da5e3ee53ba
Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2025-01-04 10:48:25 -0700

    refactor: handle generic cases w/ formatted String

--- crates/exercism/src/bottles.rs
+++ crates/exercism/src/bottles.rs
@@ -1,11 +1,11 @@
 pub fn verse(num: u32) -> String {
     match num {
-        99 => "99 bottles of beer on the wall, 99 bottles of beer.\n\
-    Take one down and pass it around, 98 bottles of beer on the wall.\n"
-            .to_string(),
-        3 => "3 bottles of beer on the wall, 3 bottles of beer.\n\
-    Take one down and pass it around, 2 bottles of beer on the wall.\n"
-            .to_string(),
+        3..=99 => format!(
+            "{0} bottles of beer on the wall, {0} bottles of beer.\n\
+            Take one down and pass it around, {1} bottles of beer on the wall.\n",
+            num,
+            num - 1
+        ),
         _ => todo!(),
     }
 }
