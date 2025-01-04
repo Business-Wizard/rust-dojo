f6d03680710a99ed5d29a92f5e0116015cd91a90
Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2025-01-04 10:50:53 -0700

    feat: selfish green of two bottles

--- crates/exercism/src/bottles.rs
+++ crates/exercism/src/bottles.rs
@@ -5,6 +5,10 @@ pub fn verse(num: u32) -> String {
             "{num} bottles of beer on the wall, {num} bottles of beer.\n\
             Take one down and pass it around, {next_num} bottles of beer on the wall.\n",
         ),
+        2 => format!(
+            "{num} bottles of beer on the wall, {num} bottles of beer.\n\
+            Take one down and pass it around, {next_num} bottle of beer on the wall.\n",
+        ),
         _ => todo!(),
     }
 }
--- crates/exercism/tests/test_bottles.rs
+++ crates/exercism/tests/test_bottles.rs
@@ -17,3 +17,12 @@ fn last_generic_verse() {
         .to_string();
     assert_eq!(expected, actual);
 }
+
+#[test]
+fn verse_2_bottles_should_handle_noun_endings() {
+    let actual = bottles::verse(2);
+    let expected = "2 bottles of beer on the wall, 2 bottles of beer.\n\
+        Take one down and pass it around, 1 bottle of beer on the wall.\n"
+        .to_string();
+    assert_eq!(expected, actual);
+}
