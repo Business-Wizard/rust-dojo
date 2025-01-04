422349d028dad76e70135baeaebcd43bacfac717
Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2025-01-04 11:00:44 -0700

    refactor: clarify count_phrase var name

--- crates/exercism/src/bottles.rs
+++ crates/exercism/src/bottles.rs
@@ -1,15 +1,15 @@
 pub fn verse(num: u32) -> String {
     let current_container = container(num);
-    let next_num = next_number_word(num);
+    let count_phrase = count_phrase(num);
     let next_container = container(num - 1);
     match num {
         2..=99 => format!(
             "{num} {current_container} of beer on the wall, {num} {current_container} of beer.\n\
-            Take one down and pass it around, {next_num} {next_container} of beer on the wall.\n"
+            Take one down and pass it around, {count_phrase} {next_container} of beer on the wall.\n"
         ),
         1 => format!(
             "{num} {current_container} of beer on the wall, {num} {current_container} of beer.\n\
-            Take it down and pass it around, {next_num} bottles of beer on the wall.\n"
+            Take it down and pass it around, {count_phrase} bottles of beer on the wall.\n"
         ),
         _ => todo!(),
     }
@@ -22,7 +22,7 @@ fn container(num: u32) -> String {
     }
 }
 
-fn next_number_word(num: u32) -> String {
+fn count_phrase(num: u32) -> String {
     match num {
         1 => "no more".to_string(),
         _ => (num - 1).to_string(),
