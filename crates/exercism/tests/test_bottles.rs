15def99e35d8fbfd42a252558aed6374a8f7c915
Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2025-01-05 09:10:01 -0700

    test: cover last three verses

--- crates/exercism/tests/test_bottles.rs
+++ crates/exercism/tests/test_bottles.rs
@@ -11,6 +11,21 @@ mod multiple_verses {
             .to_string();
         assert_eq!(expected, actual);
     }
+
+    #[test]
+    fn last_three_verses() {
+        let actual = bottles::sing(3, 0);
+        let expected = "3 bottles of beer on the wall, 3 bottles of beer.\n\
+            Take one down and pass it around, 2 bottles of beer on the wall.\n\n\
+            2 bottles of beer on the wall, 2 bottles of beer.\n\
+            Take one down and pass it around, 1 bottle of beer on the wall.\n\n\
+            1 bottle of beer on the wall, 1 bottle of beer.\n\
+            Take it down and pass it around, no more bottles of beer on the wall.\n\n\
+            No more bottles of beer on the wall, no more bottles of beer.\n\
+            Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+            .to_string();
+        assert_eq!(expected, actual);
+    }
 }
 
 mod single_verses {
